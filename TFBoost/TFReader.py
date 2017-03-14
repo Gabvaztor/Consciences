f2ce8e63f50bb44f4fb92b288407f473ab8a0e31
John Doe <gabvaztor@gmail.com>
2017-03-14 10:15:52 +0100

    Splliting validation set

--- TFBoost/TFReader.py
+++ TFBoost/TFReader.py
@@ -110,22 +110,30 @@ class Reader(object):
         trainSize = self.rFeatures.trainValidationTestPercentage[0]
         testSize = self.rFeatures.trainValidationTestPercentage[-1]
         validationSize = None
-        if len(self.rFeatures.trainValidationTestPercentage) is 3:
-            validationSize = self.rFeatures.trainValidationTestPercentage[1]
 
-        x_train_validation, x_test, y_train_validation, y_test  = train_test_split(inputData,labelData,test_size = testSize )
-        #self.trainSet,self.testSet = train_test_split(self.data, test_size = 0.2)
+        self.x_train, self.x_test, self.y_train, self.y_test  = train_test_split(inputData,labelData,test_size = testSize )
+        # self.trainSet,self.testSet = train_test_split(self.data, test_size = 0.2)
 
         # TODO Split x_train_validation and y_train_validation into train and validation if it is necessary
+        if len(self.rFeatures.trainValidationTestPercentage) is 3:  # If it has validation percentage
+            validationSize = self.rFeatures.trainValidationTestPercentage[1]  # Get validation percentage
+            totalLen = self.data.shape[0]  # All data rows
+            trainValidationLen = self.x_train.shape[0] # All train validation rows
+            #TODO Finish this
+            #TODO If the data is in columns, we have to take the shape[1] value.
+
+
+            self.x_train, self.x_validation, self.y_train, self.y_validation\
+                = train_test_split(self.x_train, self.y_train, test_size=validationSize)
 
         pt("labelData", labelData.shape)
         pt("inputData", inputData.shape)
-        pt("x_test", x_test.shape)
-        pt("y_test", y_test.shape)
-        pt("x_train_validation", x_train_validation.shape)
-        pt("y_train_validation", y_train_validation.shape)
-        pt("y_train_validation", y_train_validation)
-        pt("y_test", y_test)
+        pt("x_test", self.x_test.shape)
+        pt("y_test", self.y_test.shape)
+        pt("x_train_validation", self.x_train.shape)
+        pt("y_train_validation", self.y_train.shape)
+        pt("y_train_validation", self.y_train)
+        pt("y_test", self.y_test)
         pt("labelData", labelData.shape)
 
 
@@ -166,6 +174,7 @@ class ReaderFeatures():
         if percentages_sets :  # If it is not None
             if type(percentages_sets) is type([]) and (len(percentages_sets) is 2 or len(percentages_sets) is 3)\
                     and all(isinstance(x, float) for x in percentages_sets):  # Must be float list
+                # TODO All values must sum 100% and validation must not be higher than train
                 self.trainValidationTestPercentage = percentages_sets
             else:
                 raise RuntimeError("In ReaderFeatures, the variable 'percentages_sets'"
