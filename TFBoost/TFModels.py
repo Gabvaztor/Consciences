9250250b06519fc174db8de92c12bcf1442cab94
gabvaztor <gabvaztor@gmail.com>
2017-03-29 23:05:42 +0200

    Create conv2d and max_pool_2x2 functions

--- TFBoost/TFModels.py
+++ TFBoost/TFModels.py
@@ -127,3 +127,10 @@ class Models():
       initial = tf.constant(0.1, shape=shape)
       return tf.Variable(initial)
 
+    def conv2d(x, W):
+        return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+
+    def max_pool_2x2(x):
+        return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
+                              strides=[1, 2, 2, 1], padding='SAME')
+
