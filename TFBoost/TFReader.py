46fc60e0132e5c420feb18b462e7d5add7f38cba
gabvaztor <gabvaztor@gmail.com>
2017-04-02 20:11:38 +0200

    Create necessary comments

--- TFBoost/TFReader.py
+++ TFBoost/TFReader.py
@@ -117,11 +117,11 @@ class Reader(object):
         label_data = self.data.pop(self.rFeatures.labelsSet)  # Data's labels
         inputData = self.data  # Input data
 
-        trainSize = self.rFeatures.trainValidationTestPercentage[0]
-        testSize = self.rFeatures.trainValidationTestPercentage[-1]
+        trainSize = self.rFeatures.trainValidationTestPercentage[0]  # 0 contains trainSize
+        testSize = self.rFeatures.trainValidationTestPercentage[-1]  # last contains testSize
         validationSize = None
 
-        self.x_train, self.x_test, self.y_train, self.y_test  = train_test_split(inputData,label_data,test_size = testSize )
+        self.x_train, self.x_test, self.y_train, self.y_test  = train_test_split(inputData,label_data,test_size = testSize )  # Divide set into train and test sets (if it has validation set, into train and validation set for the first part and test set for the second part)
 
         if self.rFeatures.thereIsValidation:  # If it has validation percentage
 
@@ -133,9 +133,10 @@ class Reader(object):
             validationSize =  valueValidationPercentage / trainValidationLen  # Update validation percentage
 
             pt("ValidationSize: ",validationSize)
+            # TODO Convert sets into Tensors
             self.x_train, self.x_validation, self.y_train, self.y_validation = train_test_split(self.x_train,
                                                                                                 self.y_train,
-                                                                                                test_size=validationSize)
+                                                                                                test_size=validationSize)  # Divide train and validation sets into two separate sets.
             # TODO If there is not train and test set with optional validation then Reader will do nothing
 
     def multipleDataFiles(self):
