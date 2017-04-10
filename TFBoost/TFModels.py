40164ff78377066e49a3b380b9112c58a675bb74
gabvaztor <gabvaztor@gmail.com>
2017-04-10 22:38:32 +0200

    Normalize image and 60% training

--- TFBoost/TFModels.py
+++ TFBoost/TFModels.py
@@ -112,27 +112,28 @@ def lineal_model_basic_with_gradient_descent(self, input, test, input_labels, te
 
 # TODO Do class object with all attributes of neuronal network (x,y,y_,accuracy,...) to, after that, create a generic
 # TODO train class or method.
+@timed
 def convolution_model(input, test, input_labels, test_labels, number_of_classes, number_of_inputs=None,
-                      learning_rate=1e-4, trains=None, type=None, validation=None,
+                      learning_rate=1e-3, trains=None, type=None, validation=None,
                       validation_labels=None, deviation=None):
     """
     Generic convolutional model
     """
 
     # TODO Create an simple but generic convolutional model to analyse sets.
-    show_info = 1 # Labels and logits info.
+    show_info = 0 # Labels and logits info.
     show_images = 0 # if True show images when show_info is True
-    suffle_data = False
+    suffle_data = True
     to_array = True  # If the images must be reshaped into an array
-    x1_rows_number = 24
-    x1_column_number = 24
+    x1_rows_number = 30
+    x1_column_number = 30
     x_columns = x1_rows_number*x1_column_number
-    x_rows_column = [24,24]
+    x_rows_column = [x1_rows_number,x1_column_number]
     kernel_size = [5, 5]  # Kernel patch size
     input_size = len(input)
     num_epoch = 100  # Epochs number
     #batch_size = int(input_size/10)+1  # Batch size
-    batch_size = int(input_size / 2) +1# Batch size
+    batch_size = 100# Batch size
     # capacity must be larger than min_after_dequeue and the amount larger
     # determines the maximum we will prefetch.
     # capacity = int(input_size / 4)
@@ -141,9 +142,9 @@ def convolution_model(input, test, input_labels, test_labels, number_of_classes,
     second_label_neurons = number_neurons(input_size, first_label_neurons, number_of_classes)  # Weight second label neurons
     third_label_neurons = number_neurons(input_size, second_label_neurons, number_of_classes)  # Weight third label neurons
     '''
-    first_label_neurons = 59
-    second_label_neurons = 80
-    third_label_neurons = 120
+    first_label_neurons = 32
+    second_label_neurons = 24
+    third_label_neurons = 60
     if not trains:
         trains = int(input_size/batch_size)+1
 
@@ -164,23 +165,18 @@ def convolution_model(input, test, input_labels, test_labels, number_of_classes,
     # First Convolutional Layer
     convolution_1 = tf.layers.conv2d(
         inputs=x_reshape,
-        filters=first_label_neurons,
+        filters=third_label_neurons,
         kernel_size=kernel_size,
         padding="same",
         activation=tf.nn.relu)
     # Pool Layer 1 and reshape images into 12x12 with pool 2x2 and strides 2x2
-    pool1 = tf.layers.max_pooling2d(inputs=convolution_1, pool_size=[2, 2], strides=2)
+    #pool1 = tf.layers.max_pooling2d(inputs=convolution_1, pool_size=[2, 2], strides=2)
     # Second Convolutional Layer
-    convolution_2 = tf.layers.conv2d(
-        inputs=pool1,
-        filters=second_label_neurons,
-        kernel_size=kernel_size,
-        padding="same",
-        activation=tf.nn.relu)
+    #convolution_2 = tf.layers.conv2d(inputs=convolution_1, filters=third_label_neurons,  kernel_size=kernel_size, padding="same")
     # Pool Layer 2 and reshape images into 6x6 with pool 2x2 and strides 2x2
-    pool2 = tf.layers.max_pooling2d(inputs=convolution_2, pool_size=[2, 2], strides=2)
+    #pool2 = tf.layers.max_pooling2d(inputs=convolution_2, pool_size=[2, 2], strides=2)
     # Dense Layer
-    pool2_flat = tf.reshape(pool2, [-1, 6 * 6 * second_label_neurons])
+    pool2_flat = tf.reshape(convolution_1, [-1, x1_rows_number * x1_column_number * third_label_neurons])
     dense = tf.layers.dense(inputs=pool2_flat, units=third_label_neurons, activation=tf.nn.relu)
     dropout = tf.nn.dropout(dense, keep_probably)
     # Readout Layer
@@ -192,8 +188,9 @@ def convolution_model(input, test, input_labels, test_labels, number_of_classes,
     cross_entropy = tf.reduce_mean(
         tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y_convolution))  # Cross entropy between y_ and y_conv
 
-    #train_step = tf.train.AdadeltaOptimizer(learning_rate).minimize(cross_entropy)  # Adadelta Optimizer (gradient descent)
-    train_step = tf.train.AdamOptimizer(learning_rate).minimize(cross_entropy)  # Adam Optimizer (gradient descent)
+    train_step = tf.train.AdadeltaOptimizer(learning_rate).minimize(cross_entropy)  # Adadelta Optimizer (gradient descent)
+    #train_step = tf.train.AdamOptimizer(learning_rate).minimize(cross_entropy)  # Adam Optimizer (gradient descent)
+    #train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(cross_entropy)  # Adam Optimizer (gradient descent)
     # Sure is axis = 1
     correct_prediction = tf.equal(tf.argmax(y_convolution, axis=1), tf.argmax(y_, axis=1))  # Get Number of right values in tensor
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))  # Get accuracy in float
@@ -227,7 +224,11 @@ def convolution_model(input, test, input_labels, test_labels, number_of_classes,
     x_batch_feed, label_batch_feed = get_data_buffer_images(input, input_labels,
                                                   batch_size=batch_size,
                                                   shuffle=suffle_data, to_type=Dictionary.gray_scale,
-                                                  x_rows_column=x_rows_column, to_array=to_array)
+                                                  x_rows_column=x_rows_column, to_array=to_array,is_test=False)
+    x_test_feed, y_test_feed = get_data_buffer_images(test, test_labels,
+                                                  batch_size=len(test),
+                                                  shuffle=False, to_type=Dictionary.gray_scale,
+                                                  x_rows_column=x_rows_column, to_array=to_array, is_test=True)
     # Session
     sess = tf.InteractiveSession()
     sess.run(tf.local_variables_initializer())
@@ -237,6 +238,7 @@ def convolution_model(input, test, input_labels, test_labels, number_of_classes,
     coord = tf.train.Coordinator()
     threads = tf.train.start_queue_runners(coord=coord)
     num_trains = 0
+    start_time = time.time()
     # TRAIN
     for epoch in range (num_epoch):
         for i in range(trains):
@@ -247,8 +249,10 @@ def convolution_model(input, test, input_labels, test_labels, number_of_classes,
             # Setting values
             feed_dict_train_50 = {x: x_batch_feed, y_: label_batch_feed, keep_probably: 0.5}
             feed_dict_train_100 = {x: x_batch_feed, y_: label_batch_feed, keep_probably: 1}
+            feed_dict_test_100 = {x: x_test_feed, y_: y_test_feed, keep_probably: 1}
             train_accuracy = accuracy.eval(feed_dict_train_100) * 100
             train_step.run(feed_dict_train_50)
+            test_accuracy = accuracy.eval(feed_dict_test_100)*100
 
             # validation_accuracy = accuracy.eval(feed_dict={x: validationPlaceholder, y_: validationLabels, keep_probably: 1.0}) * 100
 
@@ -265,24 +269,27 @@ def convolution_model(input, test, input_labels, test_labels, number_of_classes,
 
                 y__ = y_.eval(feed_dict_train_100)
                 argmax_labels_y_ = [np.argmax(m) for m in y__]
-                pt('y__', argmax_labels_y_)
+                #pt('y__', argmax_labels_y_)
                 pt('y__shape', y__.shape)
                 pt('argmax_labels_y__', argmax_labels_y_)
+                pt('y__[-1]', y__[-1])
 
                 y__conv = y_convolution.eval(feed_dict_train_100)
                 argmax_labels_y_convolutional = [np.argmax(m) for m in y__conv]
                 pt('argmax_y_conv', argmax_labels_y_convolutional)
                 pt('y_conv_shape', y__conv.shape)
-                pt('y__conv', y__conv)
-
-                if show_images and i%100 == 0 and i != 0:
-                    show_image_from_tensor(x_batch_feed) # shows images
+                #pt('y__conv', y__conv)
+                pt('index_buffer_data', index_buffer_data)
+                if show_images and i == 0:
+                    show_image_from_tensor(x_batch_feed,x_rows_column) # shows images
             percent_avance = str(i*100/trains)
-            pt('TRAIN NUMBER: '+str(num_trains) + ' | Percent Epoch ' + str(epoch+1) + ": " + percent_avance + '%')
+            pt('Seconds', (time.time() - start_time))
+            pt('TRAIN NUMBER: '+str(num_trains+1) + ' | Percent Epoch ' + str(epoch+1) + ": " + percent_avance + '%')
             num_trains+=1
             pt('train_accuracy',train_accuracy)
             pt('crossEntropyTrain',crossEntropyTrain)
-            pt('index_buffer_data',index_buffer_data)
+            pt('test_accuracy',test_accuracy)
+
             # pt('correct_prediction',correct_prediction.eval())
             #pt('y_conv',y_convolution.eval(feed_dict={x: x_batch_feed, y_: label_batch_feed, keep_probably: 1.0}) * 100)
             # TODO Change to get without arguments (DO CLASS)
@@ -291,6 +298,7 @@ def convolution_model(input, test, input_labels, test_labels, number_of_classes,
                                                                 batch_size=batch_size,
                                                                 shuffle=suffle_data, to_type=Dictionary.gray_scale,
                                                                 x_rows_column=x_rows_column, to_array=to_array)
+
     # When finish coord
     coord.request_stop()
     coord.join(threads)
@@ -311,11 +319,11 @@ def read_from_reader_signal_competition(filename_queue,x_rows_column):
     return flatten_image, input_label
 
 def weight_variable(shape):
-    initial = tf.truncated_normal(shape, stddev=0.1)
+    initial = tf.truncated_normal(shape, stddev=0.01)
     return tf.Variable(initial)
 
 def bias_variable(shape):
-    initial = tf.constant(0.1, shape=shape)
+    initial = tf.constant(0.01, shape=shape)
     return tf.Variable(initial)
 
 def conv2d(x, W):
@@ -335,10 +343,10 @@ def number_neurons(total_input_size,input_sample_size,output_size,alpha=1):
     """
     return int(total_input_size/(alpha*(input_sample_size+output_size)))
 
-def show_image_from_tensor(images):
+def show_image_from_tensor(images,x_rows_column):
     """Shows images from tensors"""
     for img_index in range(images.shape[0]):
-        img = images[img_index].reshape([24,24])
+        img = images[img_index].reshape([x_rows_column[0],x_rows_column[1]])
         pt("images", img.shape)
         cv2.imshow('image', img)
         cv2.waitKey(0)  # Wait until press key to destroy image
@@ -346,7 +354,7 @@ def show_image_from_tensor(images):
 
 
 def get_data_buffer_images(inputs, inputs_labels, batch_size, shuffle=False,to_type=None,x_rows_column=None,
-                           to_array=False):
+                           to_array=False,is_test=False):
     """
     Return a x_input and y_labels with a batch_size and the same order.
     Inputs and inputs_label must have same shape.
@@ -362,6 +370,7 @@ def get_data_buffer_images(inputs, inputs_labels, batch_size, shuffle=False,to_t
     """
     # TODO DOCS
     # TODO, change when class
+    # TODO, Another method when test
     global index_buffer_data
     global inputs_processed, labels_processed
     x_inputs = []
@@ -372,24 +381,23 @@ def get_data_buffer_images(inputs, inputs_labels, batch_size, shuffle=False,to_t
     if to_type is not None:
         if to_type == Dictionary.gray_scale:
             image_type = cv2.IMREAD_GRAYSCALE
-    if shuffle and index_buffer_data == 0:
-        pt("SHUFFLE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-        c = list(zip(inputs, inputs_labels))
-        random.shuffle(c)
-        inputs_processed, labels_processed = zip(*c)
-    elif index_buffer_data == 0:
-        inputs_processed, labels_processed = inputs, inputs_labels
-    # TODO Change if array
-    if len(inputs) - index_buffer_data == 0: # When is all inputs
-        out_range = True
-    elif len(inputs) - index_buffer_data < batch_size:
-        batch_size = len(inputs) - index_buffer_data
-        out_range = True
+    if not is_test:
+        if shuffle and index_buffer_data == 0:
+            c = list(zip(inputs, inputs_labels))
+            random.shuffle(c)
+            inputs_processed, labels_processed = zip(*c)
+        elif index_buffer_data == 0:
+            inputs_processed, labels_processed = inputs, inputs_labels
+        # TODO Change if array
+        if len(inputs) - index_buffer_data == 0: # When is all inputs
+            out_range = True
+        elif len(inputs) - index_buffer_data < batch_size:
+            batch_size = len(inputs) - index_buffer_data
+            out_range = True
     for _ in range(batch_size):
-        # Load an color image in grayscale (0 is gray scale)
-        img = cv2.imread(inputs_processed[index_buffer_data], image_type)
         # Reshape
         if x_rows_column is not None:
+            img = preprocess_image(inputs_processed[index_buffer_data],image_type)
             img = cv2.resize(img,(x_rows_column[0],x_rows_column[1]))
         if to_array:
             img = img.reshape(-1)
@@ -398,9 +406,30 @@ def get_data_buffer_images(inputs, inputs_labels, batch_size, shuffle=False,to_t
         index_buffer_data+=1
     x_inputs = np.asarray(x_inputs)
     y_labels = np.asarray(y_labels)
-    pt("x_inputs",x_inputs.shape)
-    pt("y_labels", y_labels.shape)
-    if out_range:  # Reset index_buffer_data
+    if out_range and not is_test:  # Reset index_buffer_data
         index_buffer_data = 0
     #TODO Check errors
     return x_inputs, y_labels
+
+def augment_brightness_camera_images(image):
+    image1 = cv2.cvtColor(image,cv2.COLOR_RGB2HSV)
+    image1 = np.array(image1, dtype = np.float64)
+    random_bright = .5+np.random.uniform()
+    image1[:,:,2] = image1[:,:,2]*random_bright
+    image1[:,:,2][image1[:,:,2]>255]  = 255
+    image1 = np.array(image1, dtype = np.uint8)
+    image1 = cv2.cvtColor(image1,cv2.COLOR_HSV2RGB)
+    return image1
+
+def preprocess_image(image, image_type):
+    """
+
+    :param image:
+    :param image_type:
+    :return:
+    """
+    # TODO Normalize image
+    image = image
+    return image
+# Load an color image in grayscale (0 is gray scale)
+img = cv2.imread(inputs_processed[index_buffer_data], image_type)
