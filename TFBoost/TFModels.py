ea11884b02da492cbff5b6ec84cf8f07f8eca9ae
Gabriel Vázquez <gabvaztor@gmail.com>
2017-05-04 18:19:00 +0200

    TFModels class added setters

--- TFBoost/TFModels.py
+++ TFBoost/TFModels.py
@@ -505,9 +505,6 @@ class TFModels():
         self._num_epoch = 100  # Epochs number
         self._batch_size = 100  # Batch size
         self._input_size = len(input)
-        # capacity must be larger than min_after_dequeue and the amount larger
-        # determines the maximum we will prefetch.
-        # self.capacity = int(self.input_size / 4)
         self._train_dropout = 0.5  # Keep probably to dropout to avoid overfitting
         self._index_buffer_data = 0  # The index for batches during training
         self._inputs_processed = []  # New inputs processed for training. (Change during shuffle)
@@ -521,7 +518,8 @@ class TFModels():
         self._second_label_neurons = 55
         self._third_label_neurons = 50
         self._num_trains_acum = 0
-
+        if not trains:
+            self._trains = int(self.input_size / self.batch_size) + 1
     def actual_configuration(self):
         """
         Return a string with actual features
@@ -562,13 +560,28 @@ class TFModels():
     def input_size(self): return self._input_size
 
     @property
+    def batch_size(self): return self._batch_size
+
+    @property
     def train_dropout(self): return self._train_dropout
 
     @property
     def index_buffer_data(self): return self._index_buffer_data
 
+    @index_buffer_data.setter
+    def index_buffer_data(self,value): self.index_buffer_data = value
+
     @property
     def labels_processed(self): return self._labels_processed
+
+    @labels_processed.setter
+    def labels_processed(self,value): self.labels_processed = value
+
+    @property
+    def inputs_processed(self): return self._inputs_processed
+
+    @inputs_processed.setter
+    def inputs_processed(self,value): self.inputs_processed = value
 
     @property
     def first_label_neurons(self): return self._first_label_neurons
@@ -580,8 +593,13 @@ class TFModels():
     def third_label_neurons(self): return self._third_label_neurons
 
     @property
-    def num_trains_acum(self): return self._num_trains_acum
+    def trains(self): return self._trains
 
+    @property
+    def num_trains_count(self): return self._num_trains_acum
+
+    @num_trains_count.setter
+    def num_trains_count(self,value): self.num_trains_acum = value
 
     def update_inputs_and_labels_shuffling(self,inputs,inputs_labels):
         """
@@ -593,3 +611,166 @@ class TFModels():
         c = list(zip(inputs, inputs_labels))
         random.shuffle(c)
         self.inputs_processed, self.labels_processed = zip(*c)
+
+    @timed
+    def convolution_model_image(self):
+        """
+        Generic convolutional model
+        """
+
+        pt('first_label_neurons', first_label_neurons)
+        pt('second_label_neurons', second_label_neurons)
+        pt('third_label_neurons', third_label_neurons)
+        pt('input_size', input_size)
+        pt('batch_size', batch_size)
+        # TODO Try python EVAL method to do multiple variable neurons
+
+        # Placeholders
+        x = tf.placeholder(tf.float32, shape=[None, x_columns])  # All images will be 24*24 = 574
+        y_ = tf.placeholder(tf.float32, shape=[None, number_of_classes])  # Number of labels
+        keep_probably = tf.placeholder(tf.float32)  # Value of dropout. With this you can set a value for each data set
+
+        x_reshape = tf.reshape(x, [-1, x1_rows_number, x1_column_number,
+                                   1])  # Reshape x placeholder into a specific tensor
+        # TODO Define shape and stddev in methods
+        # First Convolutional Layer
+        convolution_1 = tf.layers.conv2d(
+            inputs=x_reshape,
+            filters=third_label_neurons,
+            kernel_size=kernel_size,
+            padding="same",
+            activation=tf.nn.relu)
+        # Pool Layer 1 and reshape images by 2
+        pool1 = tf.layers.max_pooling2d(inputs=convolution_1, pool_size=[2, 2], strides=2)
+        # Second Convolutional Layer
+
+        convolution_2 = tf.layers.conv2d(
+            inputs=pool1,
+            filters=third_label_neurons,
+            kernel_size=kernel_size,
+            padding="same",
+            activation=tf.nn.relu)
+
+        # # Pool Layer 2 nd reshape images by 2
+        pool2 = tf.layers.max_pooling2d(inputs=convolution_2, pool_size=[2, 2], strides=2)
+        # Dense Layer
+        # TODO Checks max pools numbers
+        pool2_flat = tf.reshape(pool2, [-1, int(x1_rows_number / 4) * int(x1_column_number / 4) * third_label_neurons])
+        dense = tf.layers.dense(inputs=pool2_flat, units=third_label_neurons, activation=tf.nn.relu)
+        dropout = tf.nn.dropout(dense, keep_probably)
+        # Readout Layer
+        w_fc2 = weight_variable([third_label_neurons, number_of_classes])
+        b_fc2 = bias_variable([number_of_classes])
+        y_convolution = (tf.matmul(dropout, w_fc2) + b_fc2)
+
+        # Evaluate model
+        cross_entropy = tf.reduce_mean(
+            tf.nn.softmax_cross_entropy_with_logits(labels=y_,
+                                                    logits=y_convolution))  # Cross entropy between y_ and y_conv
+
+        # train_step = tf.train.AdadeltaOptimizer(learning_rate).minimize(cross_entropy)  # Adadelta Optimizer (gradient descent)
+        train_step = tf.train.AdamOptimizer(learning_rate).minimize(
+            cross_entropy)  # Adam Optimizer (gradient descent)  # 97-98 test
+        # train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(cross_entropy)  # Adam Optimizer (gradient descent)
+        # Sure is axis = 1
+        correct_prediction = tf.equal(tf.argmax(y_convolution, axis=1),
+                                      tf.argmax(y_, axis=1))  # Get Number of right values in tensor
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))  # Get accuracy in float
+        # Batching values and labels from input and labels (with batch size)
+        x_batch_feed, label_batch_feed = get_data_buffer_images(input, input_labels,
+                                                                batch_size=batch_size,
+                                                                shuffle=shuffle_data, to_type=Dictionary.gray_scale,
+                                                                x_rows_column=x_rows_column, to_array=to_array,
+                                                                is_test=False)
+        x_test_feed, y_test_feed = get_data_buffer_images(test, test_labels,
+                                                          batch_size=len(test),
+                                                          shuffle=False, to_type=Dictionary.gray_scale,
+                                                          x_rows_column=x_rows_column, to_array=to_array, is_test=True)
+        # Session
+        sess = tf.InteractiveSession()
+        sess.run(tf.local_variables_initializer())
+        sess.run(tf.global_variables_initializer())
+        saver = tf.train.Saver()
+        # initialize the queue threads to start to shovel data
+        # coord = tf.train.Coordinator()
+        # threads = tf.train.start_queue_runners(coord=coord)
+
+        start_time = time.time()
+        num_trains_acum = 0
+        # TRAIN
+        for epoch in range(num_epoch):
+            for i in range(trains):
+                '''
+                Test
+                '''
+
+                # Setting values
+                feed_dict_train_50 = {x: x_batch_feed, y_: label_batch_feed, keep_probably: train_dropout}
+                feed_dict_train_100 = {x: x_batch_feed, y_: label_batch_feed, keep_probably: 1}
+                feed_dict_test_100 = {x: x_test_feed, y_: y_test_feed, keep_probably: 1}
+                train_accuracy = accuracy.eval(feed_dict_train_100) * 100
+                train_step.run(feed_dict_train_50)
+                test_accuracy = accuracy.eval(feed_dict_test_100) * 100
+
+                # validation_accuracy = accuracy.eval(feed_dict={x: validationPlaceholder, y_: validationLabels, keep_probably: 1.0}) * 100
+
+                crossEntropyTrain = cross_entropy.eval(feed_dict_train_100)
+                # crossEntropyValidation = cross_entropy.eval( feed_dict={x: validationPlaceholder, y_: validationLabels, keep_probably: 1.0})
+                # pt('conv1',tf.argmax(y_convolution, axis=1).eval(feed_dict_train_100))
+                y_pt = tf.argmax(y_, axis=1).eval(feed_dict_train_100)
+                # first_image = convolution_1.eval(feed_dict_train_100)[0]
+                # pt('convolution_1[shape]',first_image.shape)
+                '''
+                if test_accuracy > 99:
+                    # TODO return best previous model to check when save new model
+                    # Save the variables to disk.
+                    save_path = saver.save(sess, "/tmp/model.ckpt")
+                    # TODO Write in text file new models with features
+                '''
+
+                # TODO Use validation set
+                # if show_info and epoch == 0 and i == 0:
+                if show_info:
+                    # pt('the image', x_batch_feed.shape)
+
+                    y__ = y_.eval(feed_dict_train_100)
+                    argmax_labels_y_ = [np.argmax(m) for m in y__]
+                    # pt('y__', argmax_labels_y_)
+                    pt('y__shape', y__.shape)
+                    pt('argmax_labels_y__', argmax_labels_y_)
+                    pt('y__[-1]', y__[-1])
+
+                    y__conv = y_convolution.eval(feed_dict_train_100)
+                    argmax_labels_y_convolutional = [np.argmax(m) for m in y__conv]
+                    pt('argmax_y_conv', argmax_labels_y_convolutional)
+                    pt('y_conv_shape', y__conv.shape)
+                    # pt('y__conv', y__conv)
+                    pt('index_buffer_data', index_buffer_data)
+                    if show_images and i == 0:
+                        show_image_from_tensor(x_batch_feed, x_rows_column)  # shows images
+                if i == 0 or i % 10 == 0:
+                    clear_console()
+                    os.system('cls')
+                    percent_avance = str(i * 100 / trains)
+                    pt('Seconds', (time.time() - start_time))
+                    pt('Time', str(time.strftime("%Hh%Mm%Ss", time.gmtime((time.time() - start_time)))))
+                    pt('TRAIN NUMBER: ' + str(num_trains_acum + 1) + ' | Percent Epoch ' + str(
+                        epoch + 1) + ": " + percent_avance + '%')
+                    pt('train_accuracy', train_accuracy)
+                    pt('crossEntropyTrain', crossEntropyTrain)
+                    pt('test_accuracy', test_accuracy)
+                num_trains_acum += 1
+                # pt('correct_prediction',correct_prediction.eval())
+                # pt('y_conv',y_convolution.eval(feed_dict={x: x_batch_feed, y_: label_batch_feed, keep_probably: 1.0}) * 100)
+                # TODO Change to get without arguments (DO CLASS)
+                # Update batchs values
+                x_batch_feed, label_batch_feed = get_data_buffer_images(input, input_labels,
+                                                                        batch_size=batch_size,
+                                                                        shuffle=shuffle_data,
+                                                                        to_type=Dictionary.gray_scale,
+                                                                        x_rows_column=x_rows_column, to_array=to_array)
+
+        # When finish coord
+        # coord.request_stop()
+        # coord.join(threads)
+        pt('END TRAINING ')
