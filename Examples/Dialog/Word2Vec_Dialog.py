f98f1618597000e9abf2a71d7ba8a0ecdf8c6554
gabvaztor <gabvaztor@gmail.com>
2018-02-09 17:31:11 +0100

    Word2Vec_Dialog V3

--- Examples/Dialog/Word2Vec_Dialog.py
+++ Examples/Dialog/Word2Vec_Dialog.py
@@ -101,7 +101,7 @@ def generate_batches(data, word2int, vocab_size):
     return np.asarray(x_input), np.asarray(y_label)
 
 
-def generate_network_and_vector(x_input, y_label, vocab_size):
+def generate_network_and_vector(x_input, y_label, vocab_size, embedding_dim):
     """
     Crea la red neuronal con TensorFlow y utiliza las entradas y los labels para entrenarla. La red consta de 3 capas:
     - Una de entrada
@@ -114,7 +114,7 @@ def generate_network_and_vector(x_input, y_label, vocab_size):
     x = tf.placeholder(tf.float32, shape=(None, vocab_size))
     y = tf.placeholder(tf.float32, shape=(None, vocab_size))
 
-    EMBEDDING_DIM = 200  # you can choose your own number # Límite 282 portatil msi 820
+    EMBEDDING_DIM = embedding_dim  # you can choose your own number # Límite 282 portatil msi 820
     W1 = tf.Variable(tf.random_normal([vocab_size, EMBEDDING_DIM]))
     b1 = tf.Variable(tf.random_normal([EMBEDDING_DIM]))  # bias
     hidden_representation = tf.add(tf.matmul(x, W1), b1)
@@ -133,7 +133,7 @@ def generate_network_and_vector(x_input, y_label, vocab_size):
     n_iters = 10000
     # train for n_iter iterations
     for _ in range(n_iters):
-        pt(cross_entropy_loss.eval(feed_dict={x: x_input, y: y_label}))
+        #pt(cross_entropy_loss.eval(feed_dict={x: x_input, y: y_label}))
         sess.run(train_op, feed_dict={x: x_input, y: y_label})
         print('Cross Entropy es', sess.run(cross_entropy_loss, feed_dict={x: x_input, y: y_label}))
 
@@ -148,115 +148,40 @@ class Word2Vec():
     word2int = {}
     int2word = {}
     vocab_size = 0
+    question_id = -1
+    name = ""
+    def to_json(self):
+        """
+        Convert TFModel class to json with properties method.
+        :param attributes_to_delete: String set with all attributes' names to delete from properties method
+        :return: sort json from class properties.
+        """
+        self_dictionary = self.__dict__.copy()
+        json_string =  json.dumps(self, default=lambda o: self_dictionary, sort_keys=True, indent=4)
+        return json_string
 
-def main(sentences, question_id):
+    def save(self, fullpath):
+        write_string_to_pathfile(json, fullpath)
+
+def main(sentences, question_id, name, full_path_to_save):
     word2vec_class = Word2Vec()
     processes_sentences = process_for_senteces(sentences)
     words = get_words_set(processes_sentences)
     word2vec_class.word2int, word2vec_class.int2word = create_dictionaries(words)
-    word2vec_class.vocab_size = len(words)
+    word2vec_class.vocab_size, word2vec_class.name = len(words), name
     data = generate_training_data(processes_sentences, question_id)
     x_input, y_label = generate_batches(data, word2vec_class.word2int, word2vec_class.vocab_size)
-    vectors = generate_network_and_vector(x_input, y_label, word2vec_class.vocab_size)
-    return {question_id:vectors}
+    vectors = generate_network_and_vector(x_input, y_label, word2vec_class.vocab_size, 100)
+    word2vec_class.words_vectors, word2vec_class.question_id = vectors, question_id
+    word2vec_class.save(fullpath=path_to_save)
+    pt("Vector estrés in word2int", vectors[word2vec_class.word2int['si']])
+
 if __name__ == '__main__':
-    dict = main(Dialog.Estres.palabras_destacadas_pregunta_1, Dialog.Estres.id_pregunta_1)
-    pt("dict", dict)
-    asd
-corpus_raw = 'He is the king . The king is royal . She is the royal queen .'
-#corpus_raw = 'Yo no tengo nada de estrés'
-# convert to lower case
-corpus_raw = corpus_raw.lower()
-
-words = []
-for word in corpus_raw.split():
-    if word != '.': # because we don't want to treat . as a word
-        words.append(word)
-words = set(words) # so that all duplicate words are removed
-word2int = {}
-int2word = {}
-vocab_size = len(words) # gives the total number of unique words
-for i,word in enumerate(words):
-    word2int[word] = i
-    int2word[i] = word
-
-pt("word2int", word2int)
-pt("int2word", int2word)
-
-raw_sentences = corpus_raw.split('.')
-pt("raw_sentences",raw_sentences)
-
-sentences = []
-for sentence in raw_sentences:
-    sentences.append(sentence.split())
-pt("sentences",sentences)
-ASD
-#training data
-data = []
-WINDOW_SIZE = 2
-for sentence in sentences:
-    for word_index, word in enumerate(sentence):
-        for nb_word in sentence[max(word_index - WINDOW_SIZE, 0) : min(word_index + WINDOW_SIZE, len(sentence)) + 1] :
-            if nb_word != word:
-                data.append([word, nb_word])
-pt("data",data)
-pt("data",len(data))
-
-# function to convert numbers to one hot vectors
-def to_one_hot(data_point_index, vocab_size):
-    temp = np.zeros(vocab_size)
-    temp[data_point_index] = 1
-    return temp
-
-x_train = [] # input word
-y_train = [] # output word
-for data_word in data:
-    x_train.append(to_one_hot(word2int[ data_word[0] ], vocab_size))
-    y_train.append(to_one_hot(word2int[ data_word[1] ], vocab_size))
-# convert them to numpy arrays
-x_train = np.asarray(x_train)
-y_train = np.asarray(y_train)
-
-pt("x_train",x_train)
-pt("y_train",y_train)
-pt("x_train.shape",x_train.shape)
-pt("y_train.shape",y_train.shape)
-pt("vocab_size",str(vocab_size))
-
-# making placeholders for x_train and y_train
-x = tf.placeholder(tf.float32, shape=(None, vocab_size))
-y_label = tf.placeholder(tf.float32, shape=(None, vocab_size))
-
-EMBEDDING_DIM = 200 # you can choose your own number # Límite 282 portatil msi 820
-W1 = tf.Variable(tf.random_normal([vocab_size, EMBEDDING_DIM]))
-b1 = tf.Variable(tf.random_normal([EMBEDDING_DIM])) #bias
-hidden_representation = tf.add(tf.matmul(x,W1), b1)
-
-W2 = tf.Variable(tf.random_normal([EMBEDDING_DIM, vocab_size]))
-b2 = tf.Variable(tf.random_normal([vocab_size]))
-prediction = tf.nn.softmax(tf.add( tf.matmul(hidden_representation, W2), b2))
-
-sess = initialize_session()
-# define the loss function:
-cross_entropy_loss = tf.reduce_mean(-tf.reduce_sum(y_label * tf.log(prediction), reduction_indices=[1]))
-
-# define the training step:
-optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.1)
-train_op = optimizer.minimize(cross_entropy_loss)
-n_iters = 10000
-# train for n_iter iterations
-for _ in range(n_iters):
-    pt(cross_entropy_loss.eval(feed_dict={x: x_train, y_label: y_train}))
-    sess.run(train_op, feed_dict={x: x_train, y_label: y_train})
-    print('loss is : ', sess.run(cross_entropy_loss, feed_dict={x: x_train, y_label: y_train}))
-
-pt("W1")
-vectors = sess.run(tf.add(W1, b1))
-pt("vectors",vectors)
-pt("vectors_shape",vectors.shape)
-
-#pt("Vector queen in word2int", vectors[ word2int['queen'] ])
-pt("Vector estrés in word2int", vectors[ word2int['estrés'] ])
+    path_to_save = "../Examples/Dialog/Corpus/"
+    json_extension = ".json"
+    main(Dialog.Estres.palabras_destacadas_pregunta_1, Dialog.Estres.id_pregunta_1, Dialog.Estres.name,
+                path_to_save+Dialog.Estres.name+str(Dialog.Estres.id_pregunta_1)+json_extension)
+    sdasd
 
 def euclidean_dist(vec1, vec2):
     return np.sqrt(np.sum((vec1-vec2)**2))
