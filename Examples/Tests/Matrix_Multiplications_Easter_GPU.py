9a0dbeae6061134a3f5e5156c621573c769d707f
gabvaztor <gabvaztor@gmail.com>
2018-05-04 09:09:14 +0200

    CPU/GPU Tests with Matrix operations

---
+++ Examples/Tests/Matrix_Multiplications_Easter_GPU.py
@@ -0,0 +1,22 @@
+import tensorflow as tf
+import tensorflow.contrib.eager as tfe
+import time
+
+tfe.enable_eager_execution()  # Easter activation
+
+def run(size):
+  with tf.device('/gpu:0'):
+      a = tf.random_uniform([size, size])
+      b = tf.random_uniform([size, size])
+      print('Size: ', a.shape)
+      for i in range(5):
+          start = time.time()
+          tf.matmul(a, b)
+          print('Runtime is %2.5f' % (time.time() - start))
+
+print('One warmup run to account for GPU initialization')
+run(10)
+start = time.time()
+for p in [500, 1000, 2000, 5000, 10000]:
+    run(p)
+print('Total Time is %2.5f' % (time.time() - start))
