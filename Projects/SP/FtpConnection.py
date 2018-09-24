afcddfa314b1746528476ed162b600e009767290
gabvaztor <gabvaztor@gmail.com>
2018-09-24 14:24:51 +0200

    Retinopathy problem v1.9.8, SIL Data 1.5.6.4 - Vis 0.1, Threading V1.1
    Generating PDF Analysis, connection BD, Generating metadata,
    configuring PIR data, creating multivariables...
    
    FTP connection v5

--- Projects/SP/FtpConnection.py
+++ Projects/SP/FtpConnection.py
@@ -2,8 +2,8 @@
     Steps:
         - Create a new array connection in the end of file
         - Set "current_data" variable with the name of your new array connection
+        - Execute in command line: python3 filePath
 
-        - You must have Python3 installed.
         NOTE: All lines that must be modified have "TODO" word
               You must have installed python3
 
@@ -25,12 +25,14 @@ import os
 class FtpConnection():
     server = None
     ip = None
-    port = 12
+    port = None
     user = None
     password = None
     attempts = 0
     actual_dir = ""
     compressed_files = []
+    ftp_folder_name = ""
+    local_folder = ""
 
     def connect(self, ip, port, user, password):
         """
@@ -42,13 +44,22 @@ class FtpConnection():
             password:
         """
         print("Trying to connect...")
+
+        if not self.ip:
+            self.ip = ip
+        if not self.port:
+            self.port = port
+        if not self.user:
+            self.user = user
+        if not self.password:
+            self.password = password
         try:
             if not port:
-                self.server = FTP(ip)
+                self.server = FTP(self.ip)
             else:
                 self.server = FTP()
-                self.server.connect(host=ip, port=port)
-            self.server.login(user=user, passwd=password)
+                self.server.connect(host=self.ip, port=self.port)
+            self.server.login(user=self.user, passwd=self.password)
             #self.server.login()
             print("Connected")
             #self.server.connect(', 21)
@@ -68,20 +79,23 @@ class FtpConnection():
         Returns:
 
         """
-        self.server.cwd(ftp_folder)
-        self.actual_dir = ftp_folder
+        if not self.ftp_folder_name:
+            self.ftp_folder_name = ftp_folder
+        if not self.local_folder:
+            self.local_folder = local_folder
+        self.server.cwd(self.ftp_folder_name)
+        self.actual_dir = self.ftp_folder_name
         filenames = self.server.nlst()  # get filenames within the directory
         print(filenames)
         for filename in filenames:
             print("Downloading: " + filename + " ...")
-            local_filename = os.path.join(local_folder, filename)
+            local_filename = os.path.join(self.local_folder, filename)
             file = open(local_filename, 'wb')
             self.server.retrbinary('RETR ' + filename, file.write)
             if local_filename[-2:] == "gz":
                 self.compressed_files.append(local_filename)
             file.close()
         #self.server.quit()
-
 
     def delete_files_from_ftp_folder(self, ftp_folder):
         """
@@ -90,8 +104,8 @@ class FtpConnection():
             tfp_folder:
         """
         try:
-            if self.actual_dir != ftp_folder:
-                self.server.cwd(ftp_folder)
+            #if self.local_folder != self.actual_dir:
+            #    self.server.cwd(self.local_folder)
             print("Folder status before delete")
             filenames = self.server.nlst()  # get filenames within the directory
             print("Deleting files from FTP folder if any...")
@@ -104,7 +118,7 @@ class FtpConnection():
             traceback.print_exc()
             self.attempts += 1
             if self.reset_connection():
-                self.delete_files_from_ftp_folder(ftp_folder=ftp_folder)
+                self.delete_files_from_ftp_folder(ftp_folder=self.ftp_folder_name)
 
     def reset_connection(self):
         success = False
@@ -155,16 +169,71 @@ class FtpConnection():
                 return False
         return True
 
-def main(ip, port, user, password, ftp_folder, local_folder):
+    def load_json(self):
+        # Change with your setting file if necessary
+        import json
+        with open("info.json") as json_data:
+            settings = json.load(json_data)
+            self.ip=settings["ftp_name"]
+            self.port=settings["port_number"]
+            self.user=settings["user_name"]
+            self.password=settings["password"]
+            self.ftp_folder_name=settings["ftp_folder_name"]
+            self.local_folder=settings["local_folder"]
+            return settings
 
+def main_manual(ip=None, port=None, user=None, password=None,
+         ftp_folder=None, local_folder=None, avoid_set=False):
+
+    if avoid_set:
+        prism_connection_data = ["prism.nacse.org", None, "anonymous", "email@email.com",
+                                 "monthly/ppt/1895/", "Downloads\\"]
+        localhost_connection = ["localhost", 9898, "", "", "", "Downloads\\"]
+        # TODO Create a new one array connection here:
+        new_connection = ["ftp_name",
+                          21,
+                          "user_name",
+                          "password",
+                          "ftp_folder_name/",
+                          "local_folder"]
+
+        "GZIp --> Unzip: 1: All files will be downloaded, unzipped and, after that, that will be deleted"
+
+        # TODO To change when change connection (in the right, set the name of your array)
+
+        current_data = new_connection
+
+        ip = current_data[0]
+        port = current_data[1]
+        user = current_data[2]
+        password = current_data[3]
+        ftp_folder = current_data[4]
+        local_folder = current_data[5]
+    else:
+
+        ftp_connection = FtpConnection()
+        ftp_connection.connect(ip, port, user, password)
+        ftp_connection.folder_to_download(ftp_folder=ftp_folder,
+                                          local_folder=local_folder)
+        if ftp_connection.unzip_files():
+            ftp_connection.delete_files_from_ftp_folder(ftp_folder=ftp_folder)
+        if ftp_connection.server:
+            ftp_connection.close_connection()
+
+def main_json():
     ftp_connection = FtpConnection()
-    ftp_connection.connect(ip, port, user, password)
-    ftp_connection.folder_to_download(ftp_folder=ftp_folder,
-                                      local_folder=local_folder)
-    if ftp_connection.unzip_files():
-        ftp_connection.delete_files_from_ftp_folder(ftp_folder=ftp_folder)
-    if ftp_connection.server:
-        ftp_connection.close_connection()
+    dicts = ftp_connection.load_json()
+    print("dicts", str(dicts))
+
+    ip = dicts["ftp_name"]
+    port = dicts["port_number"]
+    user = dicts["user_name"]
+    password = dicts["password"]
+    ftp_folder = dicts["ftp_folder_name"]
+    local_folder = dicts["local_folder"]
+
+    main_manual(ip=ip, port=port, user=user, password=password,
+         ftp_folder=ftp_folder, local_folder=local_folder)
 
 if __name__ == "__main__":
     """
@@ -179,30 +248,6 @@ if __name__ == "__main__":
     If any downloaded file has "gz" extension, it will be uncompressed in the same folder and, after that, 
     will be deleted
     """
-
-    prism_connection_data = ["prism.nacse.org", None, "anonymous", "email@email.com",
-                             "monthly/ppt/1895/", "Downloads\\"]
-    localhost_connection = ["localhost", 9898, "", "", "", "Downloads\\"]
-    # TODO Create a new one array connection here:
-    new_connection = ["ftp_name",
-                      port number,
-                      "user_name",
-                      "password",
-                      "ftp_folder_name/",
-                      "local_folder"]
-
-    "GZIp --> Unzip: 1: All files will be downloaded, unzipped and, after that, that will be deleted"
-
-    # TODO To change when change connection (in the right, set the name of your array)
-    current_data = localhost_connection
-
-    ip = current_data[0]
-    port = current_data[1]
-    user = current_data[2]
-    password = current_data[3]
-    ftp_folder = current_data[4]
-    local_folder = current_data[5]
-
-    main(ip=ip, port=port, user=user, password=password,
-         ftp_folder=ftp_folder, local_folder=local_folder)
-
+    #main(ip=ip, port=port, user=user, password=password,
+    #     ftp_folder=ftp_folder, local_folder=local_folder)
+    main_json()
